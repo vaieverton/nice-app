99e7c0e77df8a127b9d827f294de2051d456d05c
vaieverton <everton.scorpion@hotmail.com>
2022-05-24 17:24:34 -0400

    deploy web to vercel

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://nice-app-server-2.herokuapp.com",
-  baseURL: "http://192.168.0.8:3333",
+  baseURL: "https://nice-app-server-2.herokuapp.com",
+  // baseURL: "http://192.168.0.8:3333",
 
 });
 
