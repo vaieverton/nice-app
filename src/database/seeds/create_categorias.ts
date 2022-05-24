0f871d2c0d0c17822aa2c51a602c9975b4d20aa3
vaieverton <everton.scorpion@hotmail.com>
2022-05-24 16:31:36 -0400

    corrected images and fixed database

--- src/database/seeds/create_categorias.ts
+++ src/database/seeds/create_categorias.ts
@@ -3,13 +3,12 @@ import Knex from 'knex';
 export async function seed( knex: Knex) {
     await knex('categorias').insert([
         {title: 'Futebol', image: 'futebol.png'},
-        {title: 'Basquete', image: 'basquete.svg'},
+        {title: 'Basquete', image: 'basquete.png'},
         {title: 'Natação', image: 'natacao.png'},
         {title: 'Vôlei', image: 'volei.png'},
-        {title: 'Skate', image: 'skate.svg'},
-        {title: '+Esportes', image: 'sports.svg'},
-        {title: 'Música', image: 'musica.svg'},
-        {title: 'Museus', image: 'museus.svg'},
-
+        {title: 'Skate', image: 'skate.png'},
+        {title: '+Esportes', image: 'sports.png'},
+        {title: 'Música', image: 'musica.png'},
+        {title: 'Museus', image: 'museus.png'},
     ]);
 }
